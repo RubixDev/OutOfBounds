1c761714078615666803ab116f9cbf65fdbad5de
RubixDev <silas.groh@t-online.de>
2022-02-24 18:21:31 +0100

    Make enemies switch direction on wall contact

--- src/scripts/Enemy.gd
+++ src/scripts/Enemy.gd
@@ -3,15 +3,25 @@ extends KinematicBody2D
 var CAMERA_WIDTH = ProjectSettings.get_setting('display/window/size/width')
 var CAMERA_HEIGHT = ProjectSettings.get_setting('display/window/size/height')
 var GRAVITY = CAMERA_HEIGHT / 20
+const SPEED = 200
 
 var velocity = Vector2()
+var facing_right = false
 
 signal touched_player(enemy)
 
 func _physics_process(_delta):
-	velocity.x = -200
+	if is_on_wall():
+		facing_right = !facing_right
+
+	if facing_right:
+		velocity.x = SPEED
+	else:
+		velocity.x = -SPEED
 	velocity.y += GRAVITY
-	velocity = move_and_slide(velocity)
+
+	velocity = move_and_slide(velocity, Vector2.UP)
+
 	for i in get_slide_count():
 		if get_slide_collision(i).collider.name == 'Player':
 			emit_signal('touched_player', self)
